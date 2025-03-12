413de8780c75b93ca8c87f44fa9cab797c79a9f7
Azat Azamat <azat.kariuly@gmail.com>
2025-03-12 20:54:37 +0900

    added admin backend url

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,38 +1,47 @@
-import { loadEnv, defineConfig, Modules } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as
+      | "shared"
+      | "worker"
+      | "server",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
   admin: {
+    backendUrl: process.env.BACKEND_URL,
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
   },
   modules: [
-    ...(process.env.REDIS_URL ? [{
-      key: Modules.EVENT_BUS,
-      resolve: '@medusajs/event-bus-redis',
-      options: {
-        redisUrl: process.env.REDIS_URL
-      }
+    {
+     resolve: "@medusajs/medusa/cache-redis",
+     options: {
+      redisUrl: process.env.REDIS_URL,
+     },
     },
     {
-      key: Modules.WORKFLOW_ENGINE,
-      resolve: '@medusajs/workflow-engine-redis',
-      options: {
-        redis: {
-          url: process.env.REDIS_URL,
-        }
+     resolve: "@medusajs/medusa/event-bus-redis",
+     options: {
+      redisUrl: process.env.REDIS_URL,
+     }
+    },
+    {
+     resolve: "@medusajs/medusa/workflow-engine-redis",
+     options: {
+      redis: {
+       url: process.env.REDIS_URL,
       }
-    }] : []),
-  ],
-})
+     }
+  }
+ ],
+});
